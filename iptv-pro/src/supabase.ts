e9814bf8eec46651ce62437781efeb53c6b97380
ezltv <ez41tv@gmail.com>
2025-12-02 13:27:13 +0000

    enb1

--- iptv-pro/src/supabase.ts
+++ iptv-pro/src/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 // Tür kontrolünü (types) geçici olarak kaldırdık
 // import { Database } from './types' 
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://zhmfglsqhjquqtdbeuvv.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpobWZnbHNxaGpxdXF0ZGJldXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NTgzNTksImV4cCI6MjA4MDIzNDM1OX0.It1Hj7ZMsERXAubF5oSnJkzD2zrzTVQVjXzQs7nOl8Y"
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL ve Key .env dosyasında bulunamadı!')
